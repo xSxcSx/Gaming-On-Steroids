6843ae76e0aa4898a9581360a928bacf756f2525
xSxcSx <v.weper@aol.de>
2016-01-22 14:43:30 +0100

    Update SxcSAIO.lua

--- SxcSAIO.lua
+++ SxcSAIO.lua
@@ -776,6 +776,39 @@ if Champs[ChampName] == true then
 	end 
 end
 
+function math.round(num, idp)
+  assert(type(num) == "number", "math.round: wrong argument types (<number> expected for num)")
+  assert(type(idp) == "number" or idp == nil, "math.round: wrong argument types (<integer> expected for idp)")
+  local mult = 10 ^ (idp or 0)
+  if num >= 0 then return math.floor(num * mult + 0.5) / mult
+  else return math.ceil(num * mult - 0.5) / mult
+  end
+end
+
+function string.split(str, delim, maxNb)
+  if not delim or delim == "" or string.find(str, delim) == nil then
+    return { str }
+  end
+  maxNb = (maxNb and maxNb >= 1) and maxNb or 0
+  local result = {}
+  local pat = "(.-)" .. delim .. "()"
+  local nb = 0
+  local lastPos
+  for part, pos in string.gmatch(str, pat) do
+    nb = nb + 1
+    if nb == maxNb then
+      result[nb] = lastPos and string.sub(str, lastPos, #str) or str
+      break
+    end
+    result[nb] = part
+    lastPos = pos
+  end
+  if nb ~= maxNb then
+    result[nb + 1] = string.sub(str, lastPos)
+  end
+  return result
+end
+
 class "AutoUpdater" -- {
   function AutoUpdater:__init(LocalVersion,UseHttps, Host, VersionPath, ScriptPath, SavePath, CallbackUpdate, CallbackNoUpdate, CallbackNewVersion, CallbackError)
     self.LocalVersion = LocalVersion
@@ -806,12 +839,12 @@ class "AutoUpdater" -- {
 
   function AutoUpdater:CreateSocket(url)
     if not self.LuaSocket then
-        self.LuaSocket = require("socket")
+      self.LuaSocket = require("socket")
     else
-        self.Socket:close()
-        self.Socket = nil
-        self.Size = nil
-        self.RecvStarted = false
+      self.Socket:close()
+      self.Socket = nil
+      self.Size = nil
+      self.RecvStarted = false
     end
     self.LuaSocket = require("socket")
     self.Socket = self.LuaSocket.tcp()
@@ -822,23 +855,23 @@ class "AutoUpdater" -- {
     self.Started = false
     self.LastPrint = ""
     self.File = ""
-end
+  end
 
   function AutoUpdater:Base64Encode(data)
     local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     return ((data:gsub('.', function(x)
-        local r,b='',x:byte()
-        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
-        return r;
+      local r,b='',x:byte()
+      for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+      return r;
     end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-        if (#x < 6) then return '' end
-        local c=0
-        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-        return b:sub(c+1,c+1)
+      if (#x < 6) then return '' end
+      local c=0
+      for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+      return b:sub(c+1,c+1)
     end)..({ '', '==', '=' })[#data%3+1])
-end
+  end
 
-function AutoUpdater:Base64Decode(data)
+  function AutoUpdater:Base64Decode(data)
     local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     data = string.gsub(data, '[^'..b..'=]', '')
     return (data:gsub('.', function(x)
@@ -852,130 +885,128 @@ function AutoUpdater:Base64Decode(data)
         for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
         return string.char(c)
     end))
-end
+  end
 
   function AutoUpdater:GetOnlineVersion()
     if self.GotScriptVersion then return end
 
     self.Receive, self.Status, self.Snipped = self.Socket:receive(1024)
     if self.Status == 'timeout' and not self.Started then
-        self.Started = true
-        self.Socket:send("GET "..self.Url.." HTTP/1.1\r\nHost: plebleaks.com\r\n\r\n")
+      self.Started = true
+      self.Socket:send("GET "..self.Url.." HTTP/1.1\r\nHost: plebleaks.com\r\n\r\n")
     end
     if (self.Receive or (#self.Snipped > 0)) and not self.RecvStarted then
-        self.RecvStarted = true
-        self.DownloadStatus = 'Downloading VersionInfo (0%)'
+      self.RecvStarted = true
+      self.DownloadStatus = 'Downloading VersionInfo (0%)'
     end
-
     self.File = self.File .. (self.Receive or self.Snipped)
     if self.File:find('</s'..'ize>') then
-        if not self.Size then
-            self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
-        end
-        if self.File:find('<scr'..'ipt>') then
-            local _,ScriptFind = self.File:find('<scr'..'ipt>')
-            local ScriptEnd = self.File:find('</scr'..'ipt>')
-            if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
-            local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
-            self.DownloadStatus = 'Downloading VersionInfo ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
-        end
+      if not self.Size then
+        self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
+      end
+      if self.File:find('<scr'..'ipt>') then
+        local _,ScriptFind = self.File:find('<scr'..'ipt>')
+        local ScriptEnd = self.File:find('</scr'..'ipt>')
+        if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
+        local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
+        self.DownloadStatus = 'Downloading VersionInfo ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
+      end
     end
     if self.File:find('</scr'..'ipt>') then
-        self.DownloadStatus = 'Downloading VersionInfo (100%)'
-        local a,b = self.File:find('\r\n\r\n')
-        self.File = self.File:sub(a,-1)
-        self.NewFile = ''
-        for line,content in ipairs(self.File:split('\n')) do
-            if content:len() > 5 then
-                self.NewFile = self.NewFile .. content
-            end
+      self.DownloadStatus = 'Downloading VersionInfo (100%)'
+      local a,b = self.File:find('\r\n\r\n')
+      self.File = self.File:sub(a,-1)
+      self.NewFile = ''
+      for line,content in ipairs(self.File:split('\n')) do
+        if content:len() > 5 then
+          self.NewFile = self.NewFile .. content
         end
-        local HeaderEnd, ContentStart = self.File:find('<scr'..'ipt>')
-        local ContentEnd, _ = self.File:find('</sc'..'ript>')
-        if not ContentStart or not ContentEnd then
-            if self.CallbackError and type(self.CallbackError) == 'function' then
-                self.CallbackError()
-            end
+      end
+      local HeaderEnd, ContentStart = self.File:find('<scr'..'ipt>')
+      local ContentEnd, _ = self.File:find('</sc'..'ript>')
+      if not ContentStart or not ContentEnd then
+        if self.CallbackError and type(self.CallbackError) == 'function' then
+          self.CallbackError()
+        end
+      else
+        self.OnlineVersion = (self:Base64Decode(self.File:sub(ContentStart + 1,ContentEnd-1)))
+        self.OnlineVersion = tonumber(self.OnlineVersion)
+        if self.OnlineVersion > self.LocalVersion then
+          if self.CallbackNewVersion and type(self.CallbackNewVersion) == 'function' then
+            self.CallbackNewVersion(self.OnlineVersion,self.LocalVersion)
+          end
+          self:CreateSocket(self.ScriptPath)
+          self.DownloadStatus = 'Connect to Server for ScriptDownload'
+          OnTick(function() self:DownloadUpdate() end)
         else
-            self.OnlineVersion = (self:Base64Decode(self.File:sub(ContentStart + 1,ContentEnd-1)))
-            self.OnlineVersion = tonumber(self.OnlineVersion)
-            if self.OnlineVersion > self.LocalVersion then
-                if self.CallbackNewVersion and type(self.CallbackNewVersion) == 'function' then
-                    self.CallbackNewVersion(self.OnlineVersion,self.LocalVersion)
-                end
-                self:CreateSocket(self.ScriptPath)
-                self.DownloadStatus = 'Connect to Server for ScriptDownload'
-                OnTick(function() self:DownloadUpdate() end)
-            else
-                if self.CallbackNoUpdate and type(self.CallbackNoUpdate) == 'function' then
-                    self.CallbackNoUpdate(self.LocalVersion)
-                end
-            end
+          if self.CallbackNoUpdate and type(self.CallbackNoUpdate) == 'function' then
+            self.CallbackNoUpdate(self.LocalVersion)
+          end
         end
-        self.GotScriptVersion = true
+      end
+      self.GotScriptVersion = true
     end
-end
+  end
 
   function AutoUpdater:DownloadUpdate()
-    if self.GotScriptUpdate then return end
+    if self.GotAutoUpdater then return end
     self.Receive, self.Status, self.Snipped = self.Socket:receive(1024)
     if self.Status == 'timeout' and not self.Started then
-        self.Started = true
-        self.Socket:send("GET "..self.Url.." HTTP/1.1\r\nHost: plebleaks.com\r\n\r\n")
+      self.Started = true
+      self.Socket:send("GET "..self.Url.." HTTP/1.1\r\nHost: plebleaks.com\r\n\r\n")
     end
     if (self.Receive or (#self.Snipped > 0)) and not self.RecvStarted then
-        self.RecvStarted = true
-        self.DownloadStatus = 'Downloading Script (0%)'
+      self.RecvStarted = true
+      self.DownloadStatus = 'Downloading Script (0%)'
     end
 
     self.File = self.File .. (self.Receive or self.Snipped)
     if self.File:find('</si'..'ze>') then
-        if not self.Size then
-            self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
-        end
-        if self.File:find('<scr'..'ipt>') then
-            local _,ScriptFind = self.File:find('<scr'..'ipt>')
-            local ScriptEnd = self.File:find('</scr'..'ipt>')
-            if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
-            local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
-            self.DownloadStatus = 'Downloading Script ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
-        end
+      if not self.Size then
+        self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
+      end
+      if self.File:find('<scr'..'ipt>') then
+        local _,ScriptFind = self.File:find('<scr'..'ipt>')
+        local ScriptEnd = self.File:find('</scr'..'ipt>')
+        if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
+        local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
+        self.DownloadStatus = 'Downloading Script ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
+      end
     end
     if self.File:find('</scr'..'ipt>') then
-        self.DownloadStatus = 'Downloading Script (100%)' 
-        local a,b = self.File:find('\r\n\r\n')
-        self.File = self.File:sub(a,-1)
-        self.NewFile = ''
-        for line,content in ipairs(self.File:split('\n')) do
-            if content:len() > 5 then
-                self.NewFile = self.NewFile .. content
-            end
+      self.DownloadStatus = 'Downloading Script (100%)'
+      local a,b = self.File:find('\r\n\r\n')
+      self.File = self.File:sub(a,-1)
+      self.NewFile = ''
+      for line,content in ipairs(self.File:split('\n')) do
+        if content:len() > 5 then
+          self.NewFile = self.NewFile .. content
         end
-        local HeaderEnd, ContentStart = self.NewFile:find('<sc'..'ript>')
-        local ContentEnd, _ = self.NewFile:find('</scr'..'ipt>')
-        if not ContentStart or not ContentEnd then
-            if self.CallbackError and type(self.CallbackError) == 'function' then
-                self.CallbackError()
-            end
-        else
-            local newf = self.NewFile:sub(ContentStart+1,ContentEnd-1)
-            local newf = newf:gsub('\r','')
-            if newf:len() ~= self.Size then
-                if self.CallbackError and type(self.CallbackError) == 'function' then
-                    self.CallbackError()
-                end
-                return
-            end
-            local newf = self:Base64Decode(newf)
-            local f = io.open(self.SavePath,"w+b")
-                f:write(newf)
-                f:close()
-                if self.CallbackUpdate and type(self.CallbackUpdate) == 'function' then
-                    self.CallbackUpdate(self.OnlineVersion,self.LocalVersion)
-                end
+      end
+      local HeaderEnd, ContentStart = self.NewFile:find('<sc'..'ript>')
+      local ContentEnd, _ = self.NewFile:find('</scr'..'ipt>')
+      if not ContentStart or not ContentEnd then
+        if self.CallbackError and type(self.CallbackError) == 'function' then
+          self.CallbackError()
         end
-        self.GotScriptUpdate = true
+      else
+        local newf = self.NewFile:sub(ContentStart+1,ContentEnd-1)
+        local newf = newf:gsub('\r','')
+        if newf:len() ~= self.Size then
+          if self.CallbackError and type(self.CallbackError) == 'function' then
+            self.CallbackError()
+          end
+          return
+        end
+    local f = io.open(self.SavePath,"w+b")
+    f:write(self:Base64Decode(newf))
+    f:close()
+    if self.CallbackUpdate and type(self.CallbackUpdate) == 'function' then
+    self.CallbackUpdate(self.OnlineVersion,self.LocalVersion)
     end
-end
+      end
+      self.GotAutoUpdater = true
+    end
+  end
   
   AutoUpdater(SxcSAIOVersion,ToUpdate.UseHttps, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
